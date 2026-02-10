2f8af99c1d945863eff7e9a2c7e63dce13540c3d
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-02-10 21:01:44 +0000

    Added template selector for homepage

---
+++ LibreSpotUWP/Selectors/HomeItemTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using SpotifyAPI.Web;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LibreSpotUWP.Selectors
+{
+    public class HomeItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate PlaylistTemplate { get; set; }
+        public DataTemplate AlbumTemplate { get; set; }
+        public DataTemplate SavedAlbumTemplate { get; set; }
+        public DataTemplate ArtistTemplate { get; set; }
+        public DataTemplate TrackTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is FullPlaylist) return PlaylistTemplate;
+            if (item is FullArtist) return ArtistTemplate;
+            if (item is FullTrack) return TrackTemplate;
+            if (item is FullAlbum) return AlbumTemplate;
+            if (item is SavedAlbum) return SavedAlbumTemplate;
+
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
