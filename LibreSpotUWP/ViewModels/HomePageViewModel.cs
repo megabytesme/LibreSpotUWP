5d987d16aa59429f73d15fb1d7b6e4a98de882e2
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-02-10 01:24:36 +0000

    Added view model for new home page

---
+++ LibreSpotUWP/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using SpotifyAPI.Web;
+
+namespace LibreSpotUWP.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public ObservableCollection<PlayHistoryItem> RecentlyPlayed { get; } = new ObservableCollection<PlayHistoryItem>();
+        public ObservableCollection<FullPlaylist> UserPlaylists { get; } = new ObservableCollection<FullPlaylist>();
+        public ObservableCollection<FullArtist> TopArtists { get; } = new ObservableCollection<FullArtist>();
+        public ObservableCollection<FullTrack> TopTracks { get; } = new ObservableCollection<FullTrack>();
+    }
+}
