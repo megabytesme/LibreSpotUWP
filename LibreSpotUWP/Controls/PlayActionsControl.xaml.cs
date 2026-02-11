31cf2afc648a38ba2a05e91531d6343ec6fd608f
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-02-11 21:01:52 +0000

    Added (initial) PlayActionsControl (for managing playback buttons)

---
+++ LibreSpotUWP/Controls/PlayActionsControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace LibreSpotUWP.Controls
+{
+    public sealed partial class PlayActionsControl : UserControl
+    {
+        public event EventHandler PlayRequested;
+        public event EventHandler ShuffleRequested;
+
+        public PlayActionsControl()
+        {
+            InitializeComponent();
+
+            BtnPlay.Click += (s, e) => PlayRequested?.Invoke(this, EventArgs.Empty);
+            BtnShuffle.Click += (s, e) => ShuffleRequested?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
